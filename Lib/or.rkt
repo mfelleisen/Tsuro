222213a6ddb9dedb8c4cc8043c9a60ecf8161abe
Matthias Felleisen <matthias@ccs.neu.edu>
2019-08-05 18:41:33 -0400

    embed tests in module+

--- Lib/or.rkt
+++ Lib/or.rkt
@@ -1,9 +1,19 @@
 #lang racket
 
-(provide or~)
+;; an or form that supports a kind of local definition of identifiers and values 
 
+(provide
+ ;; SYNTAX
+ ;; e = ... | (or~ {e | #:let x e}*}
+ ;; a #:let x e phrase evaluates e, binds the value to x, and
+ ;; evaluates the rest of the sub-expressions in this lexical context
+ or~)
+
+;; -----------------------------------------------------------------------------
 (require (for-syntax syntax/parse))
+(module+ test (require rackunit))
 
+;; -----------------------------------------------------------------------------
 (define-syntax (or~ stx)
   (syntax-parse stx
     [(_)
@@ -13,7 +23,8 @@
     [(_ e1 e2 ...)
      #'(or e1 (or~ e2 ...))]))
 
-(or~)
-(or~ #:let x 1 x)
-(or~ #f #:let x #f x)
-(or~ #f #:let x #t x #f)
+(module+ test 
+  (check-equal? (or~) (or))
+  (check-equal? (or~ #:let x 1 x) (or 1))
+  (check-equal? (or~ #f #:let x #f x) (or #f #f))
+  (check-equal? (or~ #f #:let x 1 x #f) (or #f 1 #f)))
