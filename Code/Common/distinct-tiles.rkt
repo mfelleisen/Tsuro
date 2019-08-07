1d24698ad55441fa201400e09fd75f89b9276ade
Matthias Felleisen <matthias@ccs.neu.edu>
2019-08-07 17:37:35 -0400

    factor out unique tile recognition

---
+++ Code/Common/distinct-tiles.rkt
@@ -0,0 +1,40 @@
+#lang racket
+
+(provide
+ all-tile-types)
+
+;; ---------------------------------------------------------------------------------------------------
+(require Tsuro/Code/Common/tiles)
+
+;; ---------------------------------------------------------------------------------------------------
+;; generate all unique tlle configurations
+
+;; see https://math.stackexchange.com/questions/1088313/how-many-different-tsuro-tiles-can-exist
+;; for a group-theoretic justification of this number 
+;; see https://en.wikipedia.org/wiki/Group_(mathematics)#Second_example:_a_symmetry_group
+;; for some basic background on symmetric groups (which is what these tiles are called)
+
+#; {-> [Setof Config]}
+;; create the set of distinnct tile configurations 
+(define (all-configs)
+  #; {[Listof Port] -> [Setof Config]}
+  ;; for the given 2 * N ports, sorted in ascending order, create partial with N edges
+  ;; GENERATIVE by removing 2 ports, we get closer to the empty case 
+  (define (all ports)
+    (cond
+      [(empty? ports) '(())]
+      [else
+       (define head (first ports))
+       (define tail (rest ports))
+       (append-map (λ (next) (map (curry cons (edge head next)) (all (remove next tail)))) tail)]))
+  
+  (for/set ((lc (all PORTS)))
+    (configuration lc)))
+
+(define all-tile-types (all-configs))
+
+(unless (= (set-count all-tile-types) 35)
+  (pretty-print all-tile-types)
+  (error 'tiles "wrong number of tiles generated: ~a" (set-count all-tile-types)))
+
+; all-tile-types
