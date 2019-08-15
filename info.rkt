85fbbf3cc0d7aec597afdd48ae79640999954da4
Matthias Felleisen <matthias@ccs.neu.edu>
2019-08-15 10:50:41 -0400

    an info file

---
+++ info.rkt
@@ -0,0 +1,21 @@
+#lang info
+
+(define collection "Tsuro")
+
+(define deps
+  '(
+    "base"
+    "racket/gui"
+    "gui-lib"
+    "at-exp-lib"
+    "typed-racket-lib"
+    "typed-racket-more"
+    "rackunit-lib"
+    "rackunit-lib"
+    ))
+
+;; json-type-provider
+
+(define pkg-desc "Sources for Tsuro/Sw Dev Fall 2019")
+
+(define pkg-authors '(matthias))
