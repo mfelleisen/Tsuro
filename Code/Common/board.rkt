2e295ef2f8bc2fb930125539af2d043fcc9909eb
Matthias Felleisen <matthias@ccs.neu.edu>
2019-08-16 21:10:46 -0400

    integrate board-dsl with board; the 'types' require it

--- Code/Common/board.rkt
+++ Code/Common/board.rkt
@@ -338,6 +338,55 @@
 ;; ---------------------------------------------------------------------------------------------------
 ;; data examples
 
+;; a "DSL" for writing down initialization and intermediate boards for tests 
+(module board-dsl racket
+  
+  (provide
+   ;; SYNTAX 
+   #; (board-from-tiles (S ...) ...)
+   #; (S =  #f
+         || I
+         || (I PlayerName _on_ Port))
+   #; {I = 0 .. TILES#}
+   ;; creates a list of tile placements from which initialze and intermediate (in board) creates states 
+   board-from-tiles on)
+
+  ;; ---------------------------------------------------------------------------------------------------
+  (require Tsuro/Code/Common/tiles)
+  (require Tsuro/Code/Common/distinct-tiles)
+  (require Tsuro/Code/Common/port-alphabetic)
+  (require (for-syntax syntax/parse))
+
+  ;; ---------------------------------------------------------------------------------------------------
+  (begin-for-syntax
+    (define-syntax-class degree [pattern 90][pattern 180][pattern 270])
+    
+    (define-syntax-class tile-index/or-tile-index-with-player
+      (pattern (~datum #f)
+               #:with square #'#f)
+      (pattern (index (~optional (~seq #:rotate r:degree) #:defaults ([r #'0])) name (~literal on) port)
+               #:declare name  (expr/c #'string?)
+               #:declare port  (expr/c #'port?)
+               #:declare index (expr/c #'(</c TILES#))
+               #:with tile   #'(rotate-tile (tile-index->tile index) #:degree r)
+               #:with square #'`(,tile ,name ,port.c))
+      (pattern index
+               #:declare index (expr/c #'(</c TILES#))
+               #:with tile   #'(tile-index->tile index.c)
+               #:with square #'`(,tile))))
+
+  (define-syntax (on stx) (raise-syntax-error 'on "used out of context" stx))
+
+  (define-syntax (board-from-tiles stx)
+    (syntax-parse stx
+      [(_ (t:tile-index/or-tile-index-with-player ...) ...)
+       #'(board-from-tiles/proc (list (list t.square ...) ...))]))
+ 
+  (define (board-from-tiles/proc rectangle)
+    (define (f row i)
+      (for/list ((cell (in-list row)) (j (in-naturals)) #:when cell)
+        (append cell (list j i))))
+    (apply append (for/list ((row (in-list rectangle)) (i (in-naturals))) (f row i)))))
 (require 'board-dsl)
 
 (define the-empty-board (build-matrix SIZE SIZE (λ (_i _j) BLANK)))
@@ -357,8 +406,7 @@
     (,tile-20 ,player-blue  ,port-4 2 0)
     (,tile-02 ,player-white ,port-3 0 2)))
 
-;; ---------------------------------------------------------------------------------------------------
-(module+ test
+(module+ test ;; testing the boar-from-tiles "dsl"
   (check-equal? (board-from-tiles
                  ((34 "red" on port-2) #f (33 #:rotate 90 "blue" on port-4))
                  (#f)
