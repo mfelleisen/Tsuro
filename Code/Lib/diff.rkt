4f9b9c952f45e90cd648acd3d6a56d9687043c55
Matthias Felleisen <matthias@ccs.neu.edu>
2019-08-24 10:48:48 +0200

    signature, improved diff, Rackety

--- Code/Lib/diff.rkt
+++ Code/Lib/diff.rkt
@@ -1,6 +1,9 @@
 #lang racket
 
-(provide diff)
+(provide
+ #;{Any Any -> (U False [List Any Any])}
+ ;; if there is a 
+ diff)
 
 (require "spy.rkt")
 
@@ -8,31 +11,36 @@
   (require rackunit))
 
 (define (diff tree1 tree2)
+
+  (define (package x y Pth)
+    (list x y (reverse Pth)))
   
-  (define (check string=? x y accu)
-    (if (string=? x y) #f (list x y accu)))
+  (define (simple string=? x y path)
+    (if (string=? x y) #f (package x y path)))
 
-  (define (check-long vector-length vector->list x y accu)
-    (or (if (= (vector-length x) (vector-length y)) #f (list x y accu))
+  (define (complex vector-length vector->list x y path)
+    (or (if (= (vector-length x) (vector-length y)) #f (package x y path))
         (for/or ((a (vector->list x)) (b (vector->list y)) (i (in-naturals)))
-          (diff a b (cons i accu)))))
+          (diff a b (cons i path)))))
 
-  (define (diff [tree1 tree1][tree2 tree2][Pth '()])
+  (define (diff [tree1 tree1][tree2 tree2][path '()])
     (match* (tree1 tree2)
       [('() '()) #f]
-      [((? number? x) (? number? y))   (check = x y Pth)]
-      [((? symbol? x) (? symbol? y))   (check eq? x y Pth)]
-      [((? string? x) (? string? y))   (check string=? x y Pth)]
-      [((? char? x)   (? char? y))     (check eq? x y Pth)]
-      [((? boolean? x) (? boolean? y)) (check eq? x y Pth)]
-      [((? vector? x) (? vector? y))   (check-long vector-length vector->list x y (cons 'vector Pth))]
-      [((? list? x)   (? list? y))     (check-long length values x y (cons 'list Pth))]
+      [((? number? x) (? number? y))   (simple = x y path)]
+      [((? symbol? x) (? symbol? y))   (simple eq? x y path)]
+      [((? string? x) (? string? y))   (simple string=? x y path)]
+      [((? char? x)   (? char? y))     (simple eq? x y path)]
+      [((? boolean? x) (? boolean? y)) (simple eq? x y path)]
+      [((? vector? x) (? vector? y))   (complex vector-length vector->list x y (cons 'vector path))]
+      [((? list? x)   (? list? y))     (complex length values x y (cons 'list path))]
       [((? struct? x) (? struct? y))
        (define xv (struct->vector x))
        (define yv (struct->vector y))
-       (diff xv yv (cons (list (vector-ref xv 0) (vector-ref yv 0)) Pth))]
+       (define x-tag (vector-ref xv 0))
+       (define y-tag (vector-ref yv 0))
+       (if (eq? x-tag y-tag) (diff xv yv (cons x-tag path)) (package x y path))]
       ;; let struct equality kick in first 
-      [((? procedure? x)   (? procedure? y)) (check eq? x y Pth)]
+      [((? procedure? x)   (? procedure? y)) (simple eq? x y path)]
       [(_   _) (list tree1 tree2)]))
 
   (diff))
