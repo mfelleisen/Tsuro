f77972937f708cca8a6c457c5c54482c5efebdfc
Matthias Felleisen <matthias@ccs.neu.edu>
2019-09-18 18:44:21 -0400

    fix method signature

--- Code/Common/player-interface.rkt
+++ Code/Common/player-interface.rkt
@@ -27,15 +27,15 @@
 
 (define referee-player%/c
   (class/c
-   [playing-as   (-> color? any)]
-   [playing-with (-> [listof color?] any)]
-   (initial      (-> initial-player-on-tile*/c tile-index? tile-index? tile-index? init-action/c))
-   [take-turn    (-> intermediate*/c tile-index? tile-index? turn-action/c)]
-   [end-of-game  (-> [listof color?] any)]))
+   [playing-as   (->m color? any)]
+   [playing-with (->m [listof color?] any)]
+   (initial      (->m initial-player-on-tile*/c tile-index? tile-index? tile-index? init-action/c))
+   [take-turn    (->m intermediate*/c tile-index? tile-index? turn-action/c)]
+   [end-of-game  (->m [listof color?] any)]))
 
 (define admin-player%/c
   (class/c
-   [end-of-tournament (-> [listof string?] any)]))
+   [end-of-tournament (->m [listof string?] any)]))
 
 (define player%/c (and/c referee-player%/c admin-player%/c))
 
