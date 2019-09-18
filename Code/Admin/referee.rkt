633af8366ad291600a8d365ec4fb7831e0314421
Matthias Felleisen <matthias@ccs.neu.edu>
2019-09-18 12:05:52 -0400

    more ideas

--- Code/Admin/referee.rkt
+++ Code/Admin/referee.rkt
@@ -1,5 +1,8 @@
 #lang racket 
 
+;; how many tiles should I provide to ensure we can play complete games?
+;; they discard up to 10 per init and up to 5 per round 
+
 ;; rules
 ;; what are the rules 
 ;; the color must be the given one 
