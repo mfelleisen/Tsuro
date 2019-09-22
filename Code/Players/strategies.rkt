2a86dbb43ca0eb34e8d3f18464416187fef1723b
Matthias Felleisen <matthias@ccs.neu.edu>
2019-09-22 18:47:13 -0400

    strategies developed with tests

--- Code/Players/strategies.rkt
+++ Code/Players/strategies.rkt
@@ -4,31 +4,53 @@
  strategy/c
 
  (contract-out
-  [first-strategy strategy/c]))
+  [first-strategy% strategy/c]))
 
 ;; ---------------------------------------------------------------------------------------------------
 (require Tsuro/Code/Common/player-interface)
+
+(module+ test
+  (require (submod ".."))
+  (require Tsuro/Code/Common/port)
+  (require rackunit))
 
 ;; ---------------------------------------------------------------------------------------------------
 (define strategy/c
   (class/c 
    (initial
-    (->m color? initial-player-on-tile*/c tile-index? tile-index? tile-index? init-action/c))
+    (->m initial-player-on-tile*/c tile-index? tile-index? tile-index? init-action/c))
    [take-turn
-    (->m color? intermediate*/c tile-index? tile-index? turn-action/c)]))
+    (->m intermediate*/c tile-index? tile-index? turn-action/c)]))
 
-(define first-strategy
+(define first-strategy%
   (class object%
 
     ;; find first free spot starting from (0,0) where a tile can be placed at the periphery
     ;; then find the first free port facing an empty tile 
-    (define/public (initial my-name-for-game tiles-placed-so-far tile1 tile2 tile3)
+    (define/public (initial tiles-placed-so-far tile1 tile2 tile3)
       (define board (initialize tiles-placed-so-far))
       (define spot  (find-first-free-spot board))
-      (cons my-name-for-game spot))
+      (cons (list tile3 0) spot))
 
     ;; use the first tile, don't rotate 
-    (define (take-turn my-name-for-game tiles-placed-so-far tile1 tile2)
+    (define/public (take-turn tiles-placed-so-far tile1 tile2)
       (list tile1 0))
 
     (super-new)))
+
+(module+ test
+  (define-syntax-rule (checks init0 spot1 (color p x y) ...)
+    (let*-values ([(strategy) (new first-strategy%)]
+                  [(init spot) (values init0 spot1)]
+                  [(init spot)
+                   (let ([c (~a 'color)])
+                     (check-equal? (send strategy initial init 1 2 3) (cons `(3 0) spot) c)
+                     (values (cons (list* tile-00 c spot) init) (list (index->port p) x y)))]
+                  ...)
+      (check-equal? (send strategy initial init 1 2 3) (cons `(3 0) spot) "last one")))
+
+  (define port-red (index->port 2))
+
+  (checks '() `(,port-red 0 0) (red 2 2 0) (black 2 4 0) (blue 2 6 0) (white 2 8 0) (green 0 9 1))
+
+  (check-equal? (send (new first-strategy%) take-turn '[] 1 2) (list 1 0)))
