3b1a2f1bc87b0bf3611cb3e734c34423e3ecdac4
Matthias Felleisen <matthias@ccs.neu.edu>
2019-10-21 15:07:10 -0400

    better name

--- Code/Common/tokens.rkt
+++ Code/Common/tokens.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
 (provide
- TOKEN-COLORS
+ AVATAR-COLORS
 
  ;; all tokens come in a distinct color
  color?)
 
 ;; -----------------------------------------------------------------------------
-(define TOKEN-COLORS '("white" "black" "red" "green" "blue")) ;; colors only
-(define (color? x) (cons? (member x TOKEN-COLORS)))
+(define AVATAR-COLORS '("white" "black" "red" "green" "blue")) ;; colors only
+(define (color? x) (cons? (member x AVATAR-COLORS)))
