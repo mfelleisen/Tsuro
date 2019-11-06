d2fb12be0516814f445f7822ed1519ffcfc379e1
Matthias Felleisen <matthias@ccs.neu.edu>
2019-11-06 10:54:00 -0500

    introduce separate interface file

---
+++ Code/Admin/observer-interfaces.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(provide
+ turn?
+ game-observer/c)
+
+(require Tsuro/Code/Common/actions)
+(require Tsuro/Code/Common/board)
+(require Tsuro/Code/Common/tokens)
+(require Tsuro/Code/Common/tiles)
+
+(define turn? [list/c [list/c avatar? turn-action/c] tile-index? tile-index?])
+(define game-observer/c
+  ;; accept information about the current state for a regular turn,
+  ;; the action requested by the active avatar, and
+  ;; the next state or #false if it is illegal
+  ;; when a player cheats, the observer is not called 
+  (-> (-> state? turn? (or/c false state?) void?)))
